7e8afc8aea936a720766f85b85b4419ed3217657
Viviana Angel <lvfandino@misena.edu.co>
2024-07-28 22:43:19 -0500

    Text about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export const About = () => {
   return (
     <div>
       <h1>About</h1>
-      <p>Esta es la página de Acerca de...</p>
+      <p>Bienvenidos a Mundo Chic, tu lugar ideal para encontrar inspiración en la moda. Nuestra tienda, establecida en 2024, nació con la misión de ofrecer ropa de alta calidad, con diseños únicos y a la moda, para ayudarlo a expresar su estilo personal.</p>
     </div>
   )
 }
